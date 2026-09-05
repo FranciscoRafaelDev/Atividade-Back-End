a6116c675bbd2f852fcfc8988d6f7d34a436a570
Francisco Rafael <clashmegae126@gmail.com>
2026-09-04 22:08:26 -0300

    Delete meu-projeto directory

--- meu-projeto/src/main/java/com/projeto/meu_projeto/service/EmailService.java
+++
@@ -1,7 +0,0 @@
-package com.projeto.meu_projeto.service;
-import com.projeto.meu_projeto.model.Pedido;
-import org.springframework.stereotype.Service;
-@Service public class EmailService { 
-    public void enviarConfirmacao(Pedido pedido) {} 
-    public void enviarCancelamento(Pedido pedido) {} 
-}
--- meu-projeto/src/main/java/com/projeto/meu_projeto/service/EstoqueService.java
+++
@@ -1,8 +0,0 @@
-package com.projeto.meu_projeto.service;
-import com.projeto.meu_projeto.model.ItemPedido;
-import org.springframework.stereotype.Service;
-import java.util.List;
-@Service public class EstoqueService { 
-    public void baixarEstoque(List<ItemPedido> itens) {} 
-    public void devolverEstoque(List<ItemPedido> itens) {} 
-}
